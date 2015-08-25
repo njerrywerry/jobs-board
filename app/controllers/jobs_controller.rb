71bf8a41d150454beb754bccd650dfbd6b87b619
Sylvia <sylviakieha@gmail.com>
2015-08-26 00:15:54 +0300

    added bootstrap and simpleform gems, scss

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,19 +1,43 @@
 class JobsController < ApplicationController
+
+  before_action :find_job, only: [:show, :edit, :update, :destroy]
+
   def index
     @jobs = Job.page(params[:page]).per(4)
   end
+
+def show
+end
 
   def new
     @job = Job.new
   end
 
   def create
-    @job = Job.new(params.require(:job).permit(:title, :company, :url))
+    @job = Job.new(jobs_params)
     if @job.save
       redirect_to root_path
     else
       render "new"
     end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def jobs_params
+   params.require(:job).permit(:title, :company, :url)
+  end
+
+  def find_job
+    @job = Job.find(params)[:id]
   end
 
 end
