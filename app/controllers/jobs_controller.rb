3edb758a1f8d47e8f5e3d57c070c1054da217cd7
Sylvia <sylviakieha@gmail.com>
2015-08-26 11:34:58 +0300

    added edit functionality

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -26,9 +26,16 @@ end
   end
 
   def update
+    if @job.update(jobs_params)
+      redirect_to @job
+    else
+      render "Edit"
+    end
   end
 
   def destroy
+    @job.destroy
+    redirect_to root_path
   end
 
   private
