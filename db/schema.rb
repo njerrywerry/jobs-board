4ed24fe4980802b845cb70014374fc365393a61e
Sylvia <sylviakieha@gmail.com>
2015-08-26 12:24:23 +0300

    generated category model and added cattegory id column to jobs

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825074228) do
+ActiveRecord::Schema.define(version: 20150826085424) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.string   "company"
     t.string   "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
 
 end
